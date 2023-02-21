1dd9b97ef100936a3b96f32f65eaf48ae6b62737
Lapxi010 <Lapxi010@yandex.ru>
2023-02-22 02:46:14 +0300

    добавилд эмодзи в чат

--- src/pages/ChatPage/ChatBox/ChatBox.tsx
+++ src/pages/ChatPage/ChatBox/ChatBox.tsx
@@ -4,8 +4,15 @@ import axios from "axios";
 import {classNames} from '@utils/classNames.js'
 import TimeAgo from 'javascript-time-ago'
 import ru from 'javascript-time-ago/locale/ru'
+import Picker from "@emoji-mart/react";
 
 TimeAgo.addDefaultLocale(ru)
+
+const smileIcon = () => {
+    return (
+        <svg className={styles.icon} xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48"><path fill={'var(--c-text)'} d="M626 523q22.5 0 38.25-15.75T680 469q0-22.5-15.75-38.25T626 415q-22.5 0-38.25 15.75T572 469q0 22.5 15.75 38.25T626 523Zm-292 0q22.5 0 38.25-15.75T388 469q0-22.5-15.75-38.25T334 415q-22.5 0-38.25 15.75T280 469q0 22.5 15.75 38.25T334 523Zm146 272q66 0 121.5-35.5T682 663H278q26 61 81 96.5T480 795Zm0 181q-83 0-156-31.5T197 859q-54-54-85.5-127T80 576q0-83 31.5-156T197 293q54-54 127-85.5T480 176q83 0 156 31.5T763 293q54 54 85.5 127T880 576q0 83-31.5 156T763 859q-54 54-127 85.5T480 976Zm0-400Zm0 340q142.375 0 241.188-98.812Q820 718.375 820 576t-98.812-241.188Q622.375 236 480 236t-241.188 98.812Q140 433.625 140 576t98.812 241.188Q337.625 916 480 916Z"/></svg>
+    )
+}
 
 export const ChatBox: FC<{ chat: any, online: any, currentUser: any, setSendMessage: any, receivedMessage: any }> = ({
                                                                                                                          chat,
@@ -19,8 +26,10 @@ export const ChatBox: FC<{ chat: any, online: any, currentUser: any, setSendMess
     const [userData, setUserData] = useState(null);
     const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState("");
+    const [showEmoji, setShowEmoji] = useState(false)
     const timeAgo = new TimeAgo('ru-RU')
     const scroll = useRef();
+    const inputRef: any = useRef();
 
     const handleChange = (newMessage) => {
         setNewMessage(newMessage.target.value)
@@ -92,6 +101,12 @@ export const ChatBox: FC<{ chat: any, online: any, currentUser: any, setSendMess
         }
     }, [receivedMessage])
 
+    const onSelect = ({native}) => {
+        setNewMessage((text) => text + native)
+        setShowEmoji(!showEmoji)
+        inputRef.current.focus()
+    }
+
     return (
         <>
             {chat ? (
@@ -122,12 +137,33 @@ export const ChatBox: FC<{ chat: any, online: any, currentUser: any, setSendMess
                     </div>
                     <div className={styles.sender}>
                         <input
+                            ref={inputRef}
                             type={'text'}
                             value={newMessage}
                             onChange={handleChange}
                             placeholder={'Наберите сообщение'}
                             className={styles.input}
                         />
+                        <div className={styles.container}>
+                            <button
+                                className={styles.btnEmoji}
+                                type='button'
+                                onClick={() => setShowEmoji(!showEmoji)}
+                            >
+                                {smileIcon()}
+                            </button>
+                            {showEmoji && (
+                                <div className={styles.container__emoji}>
+                                    <Picker
+                                        onEmojiSelect={onSelect}
+                                        emojiSize={20}
+                                        showPreview={false}
+                                        perLine={6}
+                                        locale={'ru'}
+                                    />
+                                </div>
+                            )}
+                        </div>
                         <button className={styles.btn} onClick={handleSend}>Отправить</button>
                     </div>
                 </div>
---
+++ src/pages/ChatPage/EmojiMart/EmojiMart.tsx
@@ -0,0 +1,8 @@
+import { Picker } from 'emoji-mart'
+import React from 'react'
+import { useCallback, useEffect } from 'react'
+
+export default function EmojiMart({ setText, messageInput }) {
+
+
+}
